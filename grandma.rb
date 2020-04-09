3908ca1ea2a0a611debd7baed9a6f8e47eca36a5
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 17:16:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string.upcase == string
     return "NO, NOT SINCE 1938!"
   else
